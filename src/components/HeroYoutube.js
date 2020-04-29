f5843aa9855f9686620a9ebb47c2a1e45ce9bd2a
Addison <addison@framer.com>
2020-04-29 18:40:25 +0200

    Add autoplay to youtube hero

--- src/components/HeroYoutube.js
+++ src/components/HeroYoutube.js
@@ -7,7 +7,7 @@ const HeroYoutube = () => {
       <iframe
         width="100%"
         height="700"
-        src="https://www.youtube.com/embed/2Y8l1antCuE"
+        src="https://www.youtube.com/embed/2Y8l1antCuE?autoplay=1"
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
