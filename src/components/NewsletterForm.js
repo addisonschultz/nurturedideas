66d8178f649cfb6927eec54c46fe7ff74b8cd2a6
Addison <addison@framer.com>
2020-05-07 23:21:38 +0200

    Update newsletter submissions

--- src/components/NewsletterForm.js
+++ src/components/NewsletterForm.js
@@ -25,95 +25,145 @@ const NewsletterForm = ({ namePlaceholder, emailPlaceholder }) => {
 
   const [checkmark, setCheckmark] = React.useState(false)
 
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  const [state, setState] = React.useState({
+    fullName: "",
+    email: "",
+  })
+
+  const [success, setSuccess] = React.useState(false)
+
+  const handleChange = e => {
+    const val = e.target.value
+    const name = e.target.name
+
+    setState(prevState => {
+      return { ...prevState, [name]: val }
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const form = e.target
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => setSuccess(true))
+      .catch(error => alert(error))
+  }
+
   return (
     <motion.div
       style={{ display: "flex", flexDirection: "column", width: "100%" }}
       variants={variants}
       intial="default"
     >
-      <form name={"Newsletter"} data-netlify="true">
-        <div
-          style={{ display: "flex", alignItems: "center", marginBottom: 10 }}
-        >
+      {success ? (
+        <div>Thanks! We'll be in contact soon.</div>
+      ) : (
+        <form name={"Newsletter"} data-netlify="true">
           <div
-            style={{
-              height: 10,
-              width: 10,
-              borderRadius: "100%",
-              backgroundColor: theme.colors.white0,
-              marginRight: 10,
-            }}
-          ></div>
-          <motion.input
-            className={"newsletter-input"}
-            placeholder={namePlaceholder}
-          ></motion.input>
-        </div>
-        <div
-          style={{ display: "flex", alignItems: "center", marginBottom: 20 }}
-        >
+            style={{ display: "flex", alignItems: "center", marginBottom: 10 }}
+          >
+            <div
+              style={{
+                height: 10,
+                width: 10,
+                borderRadius: "100%",
+                backgroundColor: theme.colors.white0,
+                marginRight: 10,
+              }}
+            ></div>
+            <motion.input
+              className={"newsletter-input"}
+              placeholder={namePlaceholder}
+              onChange={handleChange}
+            ></motion.input>
+          </div>
           <div
+            style={{ display: "flex", alignItems: "center", marginBottom: 20 }}
+          >
+            <div
+              style={{
+                height: 10,
+                width: 10,
+                marginRight: 10,
+                borderBottom: `10px solid ${theme.colors.white0}`,
+                borderLeft: "10px solid transparent",
+              }}
+            ></div>
+            <motion.input
+              className={"newsletter-input"}
+              placeholder={emailPlaceholder}
+              onChange={handleChange}
+            ></motion.input>
+          </div>
+          <div
+            style={{ float: "left", position: "relative", marginBottom: 20 }}
+          >
+            <label class="checkbox">
+              <input
+                type="checkbox"
+                onChange={() => setCheckmark(!checkmark)}
+              />
+              <span class="overlay">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="icon"
+                >
+                  <polyline points="20 6 9 17 4 12" />
+                </svg>
+              </span>
+            </label>
+            <motion.h3
+              style={{
+                color: theme.colors.white1,
+                fontFamily: theme.font.georgia,
+                fontWeight: 400,
+                fontSize: theme.fontSize[2],
+                paddingLeft: theme.spacing[6],
+              }}
+            >
+              I accept the terms and conditions
+            </motion.h3>
+          </div>
+          <motion.button
+            id={"newsletter-submit-button"}
+            variants={buttonVariants}
+            initial={"default"}
+            whileHover={checkmark ? "hover" : "disabled"}
+            animate={checkmark ? "default" : "disabled"}
+            transition={{ duration: 0.3 }}
+            disabled={!checkmark}
             style={{
-              height: 10,
-              width: 10,
-              marginRight: 10,
-              borderBottom: `10px solid ${theme.colors.white0}`,
-              borderLeft: "10px solid transparent",
-            }}
-          ></div>
-          <motion.input
-            className={"newsletter-input"}
-            placeholder={emailPlaceholder}
-          ></motion.input>
-        </div>
-        <div style={{ float: "left", position: "relative", marginBottom: 20 }}>
-          <label class="checkbox">
-            <input type="checkbox" onChange={() => setCheckmark(!checkmark)} />
-            <span class="overlay">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="10"
-                height="10"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="icon"
-              >
-                <polyline points="20 6 9 17 4 12" />
-              </svg>
-            </span>
-          </label>
-          <motion.h3
-            style={{
-              color: theme.colors.white1,
-              fontFamily: theme.font.georgia,
-              fontWeight: 400,
-              fontSize: theme.fontSize[2],
-              paddingLeft: theme.spacing[6],
+              width: "100%",
+              padding: 10,
             }}
           >
-            I accept the terms and conditions
-          </motion.h3>
-        </div>
-        <motion.button
-          id={"newsletter-submit-button"}
-          variants={buttonVariants}
-          initial={"default"}
-          whileHover={checkmark ? "hover" : "disabled"}
-          animate={checkmark ? "default" : "disabled"}
-          transition={{ duration: 0.3 }}
-          disabled={!checkmark}
-          style={{
-            width: "100%",
-            padding: 10,
-          }}
-        >
-          Subscribe
-        </motion.button>
-      </form>
+            Subscribe
+          </motion.button>
+        </form>
+      )}
     </motion.div>
   )
 }
