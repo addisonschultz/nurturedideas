b52b71af227b3f8a873137f5a44afd8384457a24
Addison <addison@framer.com>
2021-01-18 20:36:31 +0100

    Update mobile css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <motion.div id={"footer-container"}>
       {/* Footer Left Items */}
-      <motion.div id={"footer-left-container"} style={{ float: "left" }}>
+      <motion.div id={"footer-left-container"}>
         <motion.h3
           id={"footer-copyright"}
           className={"footer-item"}
@@ -27,7 +27,7 @@ const Footer = () => {
       </motion.div>
 
       {/* Footer Right Items */}
-      <motion.div id={"footer-right-container"} style={{ float: "right" }}>
+      <motion.div id={"footer-right-container"}>
         <motion.h3
           id={"footer-contact"}
           className={"footer-item"}
