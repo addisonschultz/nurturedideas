0d7a8c9eedf425bef282024d58544d92bc1e253c
Addison <addison@framer.com>
2021-01-19 21:20:39 +0100

    replace hydration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,9 @@ import Layout from "./src/components/layout/layout"
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
+
+export function replaceHydrateFunction() {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
