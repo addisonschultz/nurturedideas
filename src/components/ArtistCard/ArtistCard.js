2ddfcb7eed6ff03d330b9c06f5115d1d562b634d
Addison <addison@framer.com>
2021-03-10 20:24:19 +0100

    remove socials from artist card

--- src/components/ArtistCard/ArtistCard.js
+++ src/components/ArtistCard/ArtistCard.js
@@ -5,9 +5,6 @@ import Img from "gatsby-image"
 
 import * as utils from "../../utils"
 
-import spotifyGreen from "../../svg/spotify-green.svg"
-import soundcloudGreen from "../../svg/soundcloud-green.svg"
-
 import "./artist-card.css"
 
 const ArtistCard = ({ artist, variants }) => {
@@ -37,26 +34,6 @@ const ArtistCard = ({ artist, variants }) => {
       <motion.h3 className={"artist-card-artist-name"}>
         {artistData.artistName}
       </motion.h3>
-      <motion.div className={"music-link-container"}>
-        <a
-          href={artistData.spotify}
-          target={"_blank"}
-          rel="noopener noreferrer"
-        >
-          <motion.div id={"spotify-link"} className={"icon-container"}>
-            <motion.img src={spotifyGreen} className={"icon"}></motion.img>
-          </motion.div>
-        </a>
-        <a
-          href={artistData.soundcloud}
-          target={"_blank"}
-          rel="noopener noreferrer"
-        >
-          <motion.div id={"soundcloud-link"} className={"icon-container"}>
-            <motion.img src={soundcloudGreen} className={"icon"}></motion.img>
-          </motion.div>
-        </a>
-      </motion.div>
     </motion.div>
   )
 }
