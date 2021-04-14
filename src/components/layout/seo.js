cee3f39002b1bd723cae10e8ca98a0b3fe8521f4
Addison <addison@framer.com>
2021-04-14 22:34:29 +0200

    add open graph suport

--- src/components/layout/seo.js
+++ src/components/layout/seo.js
@@ -53,6 +53,10 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
+          property: `og:image`,
+          content: site.siteMetadata.image,
+        },
+        {
           name: `twitter:card`,
           content: `summary`,
         },
