ba9e3bb841b511ac445416c4fdb8a5d3ddd26426
Addison <addison@framer.com>
2021-04-16 14:01:42 +0200

    add logo to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,25 @@
 import React from "react"
 import Navigation from "../components/Navigation/Navigation"
 import SEO from "../components/layout/seo"
+import nurturedIdeas from "../images/nurtured-ideas.png"
 
 const NotFoundPage = () => (
   <>
     <Navigation />
     <SEO title="404: Not found" />
-    <div style={{ height: "100vh", display: "flex" }}>
-      <h1 style={{ margin: "auto auto" }} className={"header"}>
-        We don't have a page for this yet!
-      </h1>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ margin: "auto auto" }}>
+        <img src={nurturedIdeas} style={{ height: 200, width: 200 }}></img>
+        <h1 className={"header"}>We don't have a page for this yet!</h1>
+      </div>
     </div>
   </>
 )
